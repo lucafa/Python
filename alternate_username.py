5d260a1d4be74dc9d908e3e573d0119820e5160b
Luca Facchin <32875729+lucafa@users.noreply.github.com>
2022-03-11 16:49:25 +0100

    Update alternate_username.py
    Create 2 variable to edit payloads repeater value

--- alternate_username.py
+++ alternate_username.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+cont_username = 5
 x = 0
 for l in range(0, 133):
-    if x < 2:
+    if x < cont_username:
         print("carlos")
         x += 1
     else:
@@ -16,12 +17,12 @@ print("")
 
 f = open('password.txt','r')
 x = 0
+count_password = 5
 for line in f:
-    if x < 1:
+    if x < count_password:
         print(line,end="")
         x += 1
     else:
         print("peter")
         print(line,end="")
         x=0
-
